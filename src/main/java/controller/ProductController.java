8116026e672ea17c58a1d7a9ed87e0e9032288d2
Peter So <peterso05168@gmail.com>
2017-09-03 16:36:47 +0800

    updated ProductController and Added productDAO

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,6 +1,12 @@
 package controller;
 
 import bean.Product;
+import dao.ProductDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
  
-	@RequestMapping(value = "/products", method = RequestMethod.GET, headers="Accept=application/json")
-	public Product getProducts() {
-		return new Product();
+	@RequestMapping(value = "/shop/featured", method = RequestMethod.GET, headers="Accept=application/json")
+	public List<Product> getFeaturedProducts() {
+		List<Product> featuredProductList = new ArrayList<Product>();
+		//HARDCODED TO TEST API ONLY.
+		Product testingPurpose = new Product();
+		featuredProductList.add(testingPurpose);
+		featuredProductList.add(testingPurpose);
+		return featuredProductList;
+	}
+	
+	@RequestMapping(value = "/shop/category_detail", method = RequestMethod.POST, headers="Accept=application/json")
+	public List<Product> getCategoryProducts(HttpServletRequest request) {
+		String categoryId = request.getParameter("category_id");
+		List<Product> categoryProductList = ProductDAO.getCategoryProducts(Integer.valueOf(categoryId));
+		//HARDCODED TO TEST API ONLY.
+		Product testingPurpose = new Product();
+		categoryProductList.add(testingPurpose);
+		categoryProductList.add(testingPurpose);
+		return categoryProductList;
 	}
 	
 }
---
+++ src/main/java/dao/ProductDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import bean.Product;
+import java.util.List;
+ 
+public class ProductDAO {
+	public static List<Product> getCategoryProducts(int categoryId) {
+		return null;
+	}
+	
+}
