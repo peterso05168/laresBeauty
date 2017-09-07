b8d6f0bab7599846e4f4221a2d018b312cbff6ec
Eric Yeung <ericqwerty0@gmail.com>
2017-09-07 22:08:27 +0800

    # WARNING: head commit changed in the meantime
    update

--- src/main/java/controller/PostController.java
+++ src/main/java/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  
 @RestController
 public class PostController {
- 
+ //23342
 	@RequestMapping(value = "/posts", method = RequestMethod.GET, headers="Accept=application/json")
 	public Post getPosts() {
 		return new Post();
