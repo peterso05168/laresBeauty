dd33c2b882b5fde37c6350dc639661192d98a2d8
Peter So <peterso05168@gmail.com>
2017-09-14 23:13:08 +0800

    testing

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
  
-@RequestMapping(value = "shop")
+@RequestMapping(value = "product")
 
 @RestController
 public class ProductController {
