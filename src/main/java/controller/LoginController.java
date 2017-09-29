99c229a8280b8877d997364b5f5854fabae11732
Eric Yeung <ericqwerty0@gmail.com>
2017-09-29 22:52:39 +0800

    123

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -92,52 +92,6 @@ public class LoginController {
 			jsonLoginResult.setCode("F");
 			jsonLoginResult.setDetail("Please Contact Support.Error Message: " + e.getMessage());
 		}
-		// try {
-		// Class.forName("com.mysql.cj.jdbc.Driver");
-		// Connection con =
-		// DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/lares_beauty?autoReconnect=true&useSSL=false",
-		// "root", "123456");
-		// if (con != null) {
-		// System.out.println("Connected to the database");
-		// }
-		// PreparedStatement pstmt = con.prepareStatement("select * from user_local_auth
-		// where username = ? and password = ?");
-		// pstmt.setString(1, username);
-		// pstmt.setString(2, password);
-		// ResultSet rs = pstmt.executeQuery();
-		// if (rs.next()) {
-		// System.out.println("User " + username + " Login Successful.");
-		// Timestamp now = new Timestamp(System.currentTimeMillis());
-		// TokenHashing newHash = new TokenHashing();
-		// String newSalt = newHash.getSalt();
-		// String newSalt2 = newHash.getSalt();
-		// System.out.println("newSalt: " + newSalt);
-		// System.out.println("newSalt2 " + newSalt2);
-		// String token = (rs.getString("user_id") + ":" + newSalt + ":" + newSalt2);
-		// String hashedToken = newHash.hash(token, newSalt);
-		// PreparedStatement update = con.prepareStatement("UPDATE user_access_token SET
-		// user_local_token = ?, token_expire_time = ?, last_access_time = ? WHERE
-		// user_id = ?;");
-		// update.setString(1, hashedToken);
-		// update.setInt(2, 5);
-		// update.setTimestamp(3, now);
-		// update.setString(4, rs.getString("user_id"));
-		// update.execute();
-		// String rawAccessToken = (rs.getString("user_id") + ":" + hashedToken);
-		// String accessToken = Base64Encryption.encryption(rawAccessToken);
-		// jsonLogin.setAccessToken(accessToken);
-		// }
-		// con.close();
-		// } catch (SQLException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (ClassNotFoundException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (Exception e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
 		return jsonLoginResult;
 	}
 
--- src/main/java/jsonobject/JSONRegister.java
+++ src/main/java/jsonobject/JSONRegister.java
@@ -2,16 +2,7 @@ package jsonobject;
 
 public class JSONRegister {
 
-	private String username;
 	private Integer userId;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
 	public Integer getUserId() {
 		return userId;
